afc7635fb804847a0e209ea2ec77446809bf735c
Mariana Trinko <marjanavv@gmail.com>
2026-02-01 17:32:33 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
@@ -9,13 +9,13 @@ class Person:
 
 def create_person_list(people: list) -> list:
     Person.people.clear()
-    person_list = [Person(p["name"], p["age"]) for p in people]
-    for p in people:
-        person = Person.people[p["name"]]
-        wife_name = p.get("wife")
+    person_list = [Person(atr["name"], atr["age"]) for atr in people]
+    for atr in people:
+        person = Person.people[atr["name"]]
+        wife_name = atr.get("wife")
         if wife_name is not None:
             person.wife = Person.people[wife_name]
-        husband_name = p.get("husband")
+        husband_name = atr.get("husband")
         if husband_name is not None:
             person.husband = Person.people[husband_name]
     return person_list
